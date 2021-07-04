0e911089241eb7608fec147d45b439540fc415ad
JoseAvellaneda19 <chemo383@gmail.com>
2021-07-04 12:42:52 +0200

    implemented userReducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './redux/store';
+
 import './index.css';
+
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+
+
 
 
 ReactDOM.render(
+  <Provider store={store}> 
+
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+
+  </Provider>,
   document.getElementById('root')
 );
 
